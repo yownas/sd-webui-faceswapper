7a2c0cfec94242d5c229fe90cfd45c27db8fd929
Jonas Stare <yownas@gmail.com>
2023-07-02 10:09:28 +0200

    Add verbose and bugfix for match

--- scripts/sd-webui-faceswapper.py
+++ scripts/sd-webui-faceswapper.py
@@ -21,6 +21,14 @@ class Script(scripts.Script):
     def __init__(self): # pylint: disable=useless-super-delegation
         super().__init__()
 
+    class Rules(object):
+        rules = ''
+        verbose = False
+        pass
+
+    def LOG(self, text):
+        print(f"Face swapper: {text}")
+
     # script title to show in ui
     def title(self):
         return 'Face swapper'
@@ -57,34 +65,35 @@ class Script(scripts.Script):
             FACE_ANALYSER.prepare(ctx_id=0, det_size=(640, 640))
         return FACE_ANALYSER
 
-    def swap_matchrules(self, img, target_img, in_faces, out_faces, swap_rules):
+    def swap_matchrules(self, img, target_img, in_faces, out_faces, swaprules):
         if len(in_faces) and len(out_faces):
             rridx = 0
-            for out_face in out_faces:
+            for out_idx in range(len(out_faces)):
+                idx = None
                 if shared.state.interrupted:
                     break
-                if 'age' in swap_rules:
+                if 'age' in swaprules.rules:
                     gap = sys.maxsize
                     for i in range(len(in_faces)):
-                        diff = abs(out_face.age - in_faces[i].age)
+                        diff = abs(out_faces[out_idx].age - in_faces[i].age)
                         if diff < gap:
                             gap = diff
                             idx = i
-                elif 'similar' in swap_rules: # Match similarity
+                elif 'similar' in swaprules.rules: # Match similarity
                     gap = 0
                     for i in range(len(in_faces)):
-                        f1 = out_face.normed_embedding
+                        f1 = out_faces[out_idx].normed_embedding
                         f2 = in_faces[i].normed_embedding
                         # Based on https://learnopencv.com/face-recognition-with-arcface/
                         sim = max(np.dot(f1, f2) / (np.sqrt(np.dot(f1, f1)) * np.sqrt(np.dot(f2, f2))), 0)
                         if sim > gap:
                             gap = sim
                             idx = i
-                elif 'ssim' in swap_rules: # Match ssim (very very experimental)
+                elif 'ssim' in swaprules.rules: # Match ssim (very very experimental)
                     SSIM = StructuralSimilarityIndexMeasure(data_range=1.0)
                     gap = 0
                     for i in range(len(in_faces)):
-                        ofb = out_face.bbox
+                        ofb = out_faces[out_idx].bbox
                         ifb = in_faces[i].bbox
                         t = transforms.Compose([transforms.ToTensor()])
                         f1 = t(Image.fromarray(img).crop((ofb[0], ofb[1], ofb[2], ofb[3])).resize((128, 128))).unsqueeze(0)
@@ -98,8 +107,10 @@ class Script(scripts.Script):
                     idx = rridx%len(in_faces)
                     rridx+=1
 
-                if idx:
-                    img = self.get_face_swapper().get(img, out_face, in_faces[idx], paste_back=True)
+                if idx is not None:
+                    if swaprules.verbose:
+                        self.LOG(f"Match: swap {out_idx+1} with {idx+1}")
+                    img = self.get_face_swapper().get(img, out_faces[out_idx], in_faces[idx], paste_back=True)
                 if shared.opts.live_previews_enable:
                     shared.state.assign_current_image(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
         return(img)
@@ -107,6 +118,8 @@ class Script(scripts.Script):
     # run at the end of sequence for always-visible scripts
     def postprocess(self, p, processed, is_enabled, replace, restore, source_face_dict, swap_rules):  # pylint: disable=arguments-differ
         if is_enabled and not shared.state.interrupted:
+            swaprules = self.Rules()
+
             if isinstance(source_face_dict["image"], str):
                 source_face = decode_base64_to_image(source_face_dict["image"])
             else:
@@ -129,15 +142,20 @@ class Script(scripts.Script):
             img_len = len(processed.images)
             with tqdm(total=img_len, desc="Face swapping", unit="image") as progress:
                 if img_len:
+                    swap_rules = re.sub(r'[\s;:|]+', r' ', swap_rules)
+                    swap_rules, cnt = re.subn(r' *verbose *', '', swap_rules)
+                    if cnt:
+                        self.LOG("Verbose mode enabled")
+                        swaprules.verbose = True
                     swap_rules = re.sub(r'(similar|same|like)', r'similar', swap_rules)
                     swap_rules = re.sub(r'(sex|gender)', r'sex', swap_rules)
                     swap_rules = re.sub(r'(age|old)', r'age', swap_rules)
-                    swap_rules = re.sub(r'[\s;:|]+', r' ', swap_rules)
                     swap_rules = re.sub(r' *([>,]) *', r'\1', swap_rules) # Remove \s around > & ,
                     swap_rules = re.sub(r' +', r' ', swap_rules)
                     swap_rules = re.sub(r'(^ | $)', r'', swap_rules) # Trim
                     if not swap_rules:
                         swap_rules = '*>*' # default rule
+                    swaprules.rules = swap_rules
 
                 for i in range(img_len):
                     if shared.state.interrupted:
@@ -149,32 +167,31 @@ class Script(scripts.Script):
                             shared.state.assign_current_image(processed.images[i])
                         faces = sorted(self.get_face_analyser().get(img), key=lambda x: x.bbox[0])
                         if faces:
-                            if 'match' in swap_rules: # "match sex age"
-                                in_f = []
-                                in_m = []
-                                out_f = []
-                                out_m = []
+                            if 'match' in swaprules.rules: # "match sex age"
+                                in_f, in_m, out_f, out_m = [], [], [], []
 
                                 # sort faces by sex
                                 for face in targets:
-                                    if "sex" in swap_rules and face.sex == "F":
+                                    if "sex" in swaprules.rules and face.sex == "F":
                                         in_f.append(face)
                                     else:
                                         in_m.append(face)
                                 for face in faces:
-                                    if "sex" in swap_rules and face.sex == "F":
+                                    if "sex" in swaprules.rules and face.sex == "F":
                                         out_f.append(face)
                                     else:
                                         out_m.append(face)
 
+                                if swaprules.verbose:
+                                    self.LOG(f"Rules: {swaprules.rules}")
                                 # Swap faces
-                                img = self.swap_matchrules(img, target_img, in_f, out_f, swap_rules)
-                                img = self.swap_matchrules(img, target_img, in_m, out_m, swap_rules)
+                                img = self.swap_matchrules(img, target_img, in_f, out_f, swaprules)
+                                img = self.swap_matchrules(img, target_img, in_m, out_m, swaprules)
                             else:
                                 # Use swap rules
                                 swap_pairs = {}
                                 rr_targets = {}
-                                for rule in swap_rules.split(' '):
+                                for rule in swaprules.rules.split(' '):
                                     in_face, out_faces = rule.split('>', 1)
                                     if out_faces == '*':
                                       if in_face == '*':
@@ -185,6 +202,8 @@ class Script(scripts.Script):
                                         for out_face in out_faces.split(','):
                                             swap_pairs[out_face] = -1 if in_face == '*' else int(in_face) 
 
+                                if swaprules.verbose:
+                                    self.LOG(f"Rules: {swaprules.rules}")
                                 rr = 0
                                 for idx in range(1, len(faces)+1):
                                     if shared.state.interrupted:
@@ -196,6 +215,8 @@ class Script(scripts.Script):
                                         rr+=1
                                     if in_face is not -1:
                                         # Swap
+                                        if swaprules.verbose:
+                                            self.LOG(f"Swap {idx} with {in_face}")
                                         img = self.get_face_swapper().get(img, faces[idx-1], targets[in_face-1], paste_back=True)
                                         if shared.opts.live_previews_enable:
                                             shared.state.assign_current_image(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
